59020e4f053dd3a9ffa289ed5511c5c596d1574f
swed1sch <swedisch97@gmail.com>
2020-02-27 19:13:04 +0300

    add Rocket script

--- Asteroids-for-Qmobi/Assets/Game/Scripts/Rocket.cs
+++ Asteroids-for-Qmobi/Assets/Game/Scripts/Rocket.cs
@@ -1,18 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public GameObject bullet;
+    private float thrust = 6f;
+    private float rotationSpeed = 180f;
+    private float MaxSpeed = 4.5f;
+    private Camera mainCam;
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        bullet.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        ControlRocket();
+        CheckPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
     }
+
+    private void ControlRocket()
+    {
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+        rb.AddForce(transform.up * thrust * Input.GetAxis("Vertical"));
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -MaxSpeed, MaxSpeed), Mathf.Clamp(rb.velocity.y, -MaxSpeed, MaxSpeed));
+    }
+
+    private void CheckPosition()
+    {
+        float sceneWidth = mainCam.orthographicSize * 2 * mainCam.aspect;
+        float sceneHeight = mainCam.orthographicSize * 2;
+
+        float sceneRightEdge = sceneWidth / 2;
+        float sceneLeftEdge = sceneWidth *-1;
+        float sceneTopEdge = sceneWidth / 2;
+        float sceneBottomEdge = sceneWidth *-1;
+
+        if (transform.position.x > sceneRightEdge)
+        {
+            transform.position = new Vector2(sceneLeftEdge , transform.position.y);
+        }
+        if (transform.position.x < sceneLeftEdge)
+        {
+            transform.position = new Vector2(sceneRightEdge, transform.position.y);
+        }
+        if (transform.position.y > sceneTopEdge)
+        {
+            transform.position = new Vector2(transform.position.x, sceneBottomEdge);
+        }
+        if (transform.position.y < sceneBottomEdge)
+        {
+            transform.position = new Vector2(transform.position.x, sceneTopEdge);
+        }
+
+    }
+    
+    void Shoot()
+    {
+        GameObject bulletClone = Instantiate(bullet, new Vector2(bullet.transform.position.x, bullet.transform.position.y), transform.rotation);
+        bulletClone.SetActive(true);
+        bulletClone.GetComponent<Bullet>().DeleteBullet();
+        bulletClone.GetComponent<Rigidbody>().AddForce(transform.up * 350);
+    }
+
 }
